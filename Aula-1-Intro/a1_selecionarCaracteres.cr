613a4c385a494f7bf8666b9c56fb747ca604efcd
guto <91899995+gutoso@users.noreply.github.com>
2022-02-26 02:41:09 -0300

    Create a1_selecionarCaracteres.cr

---
+++ Aula-1-Intro/a1_selecionarCaracteres.cr
@@ -0,0 +1,9 @@
+text = "The black cat climbed the green three"
+
+puts text[0] # Seleciona o primeiro caractere, pois o "0" representa a primeira posição
+
+puts text[0, 4] # Seleciona os cinco primeiros caracteres
+
+puts text[0..5] # Seleciona os caracteres que vão da primeira casa até a quinta casa, ou seja, os seis primeiros caracteres
+
+puts text[0..-3] # Seleciona todos os caracteres, exceto os três que estão nas últimas três casas
