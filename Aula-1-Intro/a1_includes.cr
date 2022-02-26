6bff0eb674d5323630a6d2010b4397f265cd3743
guto <91899995+gutoso@users.noreply.github.com>
2022-02-26 02:43:24 -0300

    Create a1_includes.cr

---
+++ Aula-1-Intro/a1_includes.cr
@@ -0,0 +1,7 @@
+text = "The black cat climbed the green three"
+
+puts text.includes?("cat") # Retorna true
+
+puts text.includes?("dog") # Retorna false
+
+# Adicionando "includes?" seguindo da sequência de caracteres que queremos verificar, podemos saber se a string contém a determinada sequência ou não, retornando true ou false
