e6af33626fce860d55e8436d55649dc88ccb1635
guto <91899995+gutoso@users.noreply.github.com>
2022-02-26 02:37:30 -0300

    Create a1_inverterString.cr

---
+++ Aula-1-Intro/a1_inverterString.cr
@@ -0,0 +1,5 @@
+text = "The black cat climbed the green three"
+
+puts text.reverse
+
+# Para invertermos a string basta adicionar ".reverse", assim como este dado pode ser armazenado em uma variável separada!
